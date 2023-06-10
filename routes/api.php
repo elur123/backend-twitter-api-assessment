ec17bcff0d9d0e293b1b69f07cc691f7a8179f7c
Ruel Israel <elurl115@gmail.com>
2023-06-10 13:26:39 +0800

    Add comments on api routes

--- routes/api.php
+++ routes/api.php
@@ -11,25 +11,25 @@ Route::prefix('v1')->name('v1.')->group(function() {
 
     Route::prefix('auth')->name('auth.')->group(function () {
 
-        Route::post('register', [AuthController::class, 'register'])->name('register');
-        Route::post('login', [AuthController::class, 'login'])->name('login');
-        Route::post('logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth:sanctum');
+        Route::post('register', [AuthController::class, 'register'])->name('register'); // Register user
+        Route::post('login', [AuthController::class, 'login'])->name('login'); // Login user
+        Route::post('logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth:sanctum'); // Logout user
     });
 
     Route::middleware('auth:sanctum')->group(function () {
 
-        Route::get('me', [UserController::class, 'showAuthenticatedUser'])->name('me');
+        Route::get('me', [UserController::class, 'showAuthenticatedUser'])->name('me'); // Get user authenticated details
 
         Route::prefix('user')->name('name.')->group(function () {
 
-            Route::get('{user}', [UserController::class, 'show'])->name('show');
-            Route::put('{user}', [UserController::class, 'update'])->name('update');
-            Route::get('{user}/follower', [UserController::class, 'follower'])->name('follower');
-            Route::get('{user}/followed', [UserController::class, 'followed'])->name('followed');
-            Route::post('{user}/follow', [UserController::class, 'follow'])->name('follow');
-            Route::post('{user}/unfollow', [UserController::class, 'unfollow'])->name('unfollow');
+            Route::get('{user}', [UserController::class, 'show'])->name('show'); // Get user details based on user uuid
+            Route::put('{user}', [UserController::class, 'update'])->name('update'); // Update user details based on user uuid
+            Route::get('{user}/follower', [UserController::class, 'follower'])->name('follower'); // Get user follower using user uuid
+            Route::get('{user}/followed', [UserController::class, 'followed'])->name('followed'); // Get user followed using user uuid
+            Route::post('{user}/follow', [UserController::class, 'follow'])->name('follow'); // Authenticated user follow user using user uuid
+            Route::post('{user}/unfollow', [UserController::class, 'unfollow'])->name('unfollow'); // Authenticated user unfollow user using user uuid
         });
 
-        Route::resource('tweets', TweetController::class);
+        Route::resource('tweets', TweetController::class); // Tweet resource route
     });
 });
